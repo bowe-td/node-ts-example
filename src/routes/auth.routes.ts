09a2063f5fd2a1fd73a319e0a3fb1f5797011d32
Caio Rodrigues <caio@ambitus.io>
2020-07-09 10:25:22 -0300

    correct implementation

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,26 +1,9 @@
 import { Router } from 'express';
-import BadRequest from '../exceptions/BadRequestException'
-
-import AuthService from '../services/AuthService';
+import AuthController from '../controllers/AuthController';
 
 const authRouter = Router();
+const controller = new AuthController();
 
-authRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
-
-    const authService = new AuthService();
-
-    const { email: user, token } = await authService.execute({
-      email,
-      password,
-    });
-
-    return response.json({ user, token });
-  }catch(e) {
-    const error = new BadRequest(e)
-    return response.status(error.code).json(error)
-  }
-});
+authRouter.post('/', controller.authUser);
 
 export default authRouter;
